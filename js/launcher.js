cfc6b7a3eab86734e71e166644817f4b5835d2c8
HarmanjeetSingh1332 <83233849+HarmanjeetSingh1332@users.noreply.github.com>
2021-08-06 20:05:38 +0530

    Add files via upload

---
+++ js/launcher.js
@@ -0,0 +1,38 @@
+class Launcher{
+	constructor(bodyA, pointB){
+		var options = {
+			bodyA: bodyA,
+			pointB: pointB,
+			stiffness: 0.04,
+			length: 10
+		}
+		this.bodyA = bodyA;
+		this.pointB = pointB;
+		this.launcher = Constraint.create(options);
+		World.add(world,this.launcher);
+	}
+
+	display(){
+		if(this.launcher.bodyA){
+			var pointA = this.launcher.bodyA.position;
+			var pointB = this.pointB;
+			strokeWeight(5);
+			line(pointA.x, pointA.y, pointB.x, pointB.y);
+		}
+	}
+	
+	fly(){
+		this.launcher.bodyA = null;
+	}
+
+	emigrant(){
+        if(this.sling.bodyA){
+          return true;
+        }
+        else {
+          return false;
+        } 
+    
+    }
+	
+}
